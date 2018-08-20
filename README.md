ea1ee44cec1b37e6dfd4da2d5873739b1a85f2bf
Robert Fox <fox.robert92@gmail.com>
2018-08-20 17:00:46 +0800

    added w1-gpio.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # foglamp-south-w1-gpio
+
+FogiLAMP South Plugin to retrive data from a 1 wire sensor via GPIO on a Raspberry Pi
+
+FogLAMP Project: https://github.com/foglamp/FogLAMP
+
+FogLAMP Docs: https://foglamp.readthedocs.io/en/master/
---
+++ w1-gpio.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+
+# FOGLAMP_BEGIN
+# See: http://foglamp.readthedocs.io/
+# FOGLAMP_END
+
+"""
+Poll based south plugin to retrieve data from w1-gpio devices on a raspberry pi,
+based on the following template: https://github.com/foglamp/FogLAMP-Examples/blob/master/south_plugin_poll/python/foglamp/plugins/south/poll_template/poll_template.py
+"""
+
+import copy
+from datetime import datetime, timezone
+import random
+import uuid
+import re
+import os
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+from foglamp.common import logger
+from foglamp.services.south import exceptions
+
+__author__ = "Robert Fox"
+__copyright__ = "Copyright (c) 2018 OSIsoft, LLC"
+__license__ = "Apache 2.0"
+__version__ = "${VERSION}"
+
+_DEFAULT_CONFIG = {
+    'plugin': {
+         'description': 'FogLAMP South plugin to pull data from a w1-gpio device on a Raspberry Pi',
+         'type': 'string',
+         'default': 'w1-gpio'
+    },
+    'pollInterval': {
+        'description': 'The interval between polling calls (in milliseconds)',
+        'type': 'integer',
+        'default': '1000'
+    }
+    'deviceList':{
+        'description':'List of w1_slave devices (comma-separated)',
+        'type':'string',
+        'default':''
+    }
+
+}
+
+_LOGGER = logger.setup(__name__)
+
+
+def plugin_info():
+    """ Returns information about the plugin
+
+    Args:
+    Returns:
+        dict: plugin information
+    Raises:
+    """
+
+    return {
+        'name': 'w1-gpio Plugin',
+        'version': '1.0',
+        'mode': 'poll', ''
+        'type': 'south',
+        'interface': '1.0',
+        'config': _DEFAULT_CONFIG
+    }
+
+
+def plugin_init(config):
+    """ Initialise the plugin.
+
+    Args:
+        config: JSON configuration document for the South plugin configuration category
+    Returns:
+        handle: JSON object to be used in future calls to the plugin
+    Raises:
+    """
+
+    handle = config
+    return handle
+
+def readFromSensor(sensorID):
+    """ Parses the file associated with a given sensor ID
+
+    Args:
+      sensorID: the GUID of the sensor
+    Returns:
+      value: read value from sensor
+    Raises:
+      ValueError: If there is no temperature found in file or if the file fails the CRC check
+    """
+    sensorFilePath = "/sys/bus/w1/devices/"+sensorID+"/w1_slave"
+    value = ""
+    with open(sensorFilePath,'r') as sensorFile:
+        sensorLines = sensorFile.readlines()
+    # Check to see if the CRC check passes, otherwise raise error
+    # only supports temperature based w1_slave returning values in 1000ths of 1 degree
+    if sensorLines[0].endswith("YES"):
+        tempStart = sensorLines[1].find("t=")
+        if tempStart != -1:
+            temp = sensorLines[1][tempStart+2:]
+            value = float(temp)/1000
+        else:
+            raise ValueError('Temperature not found in file')
+    else:
+        raise ValueError('File Fails CRC Check')
+
+    return value
+
+def plugin_poll(handle):
+    """ Extracts data from the sensor and returns it in a JSON document as a Python dict.
+
+    Args:
+        handle: handle returned by the plugin initialisation call
+    Returns:
+        returns a sensor reading in a JSON document;
+        A Python dict - if it is available,
+        None - If no reading is available
+    Raises:
+        DataRetrievalError
+    """
+
+    timestamp = str(datetime.now(tz=timezone.utc))
+
+    try:
+        data = {
+                'asset': 'poll_template',
+                'timestamp': timestamp,
+                'key': str(uuid.uuid4()),
+                'readings':  {'value': random.randint(0, 1000)}
+        }
+
+    except Exception as ex:
+        raise exceptions.DataRetrievalError(ex)
+
+    return data
+
+
+def plugin_reconfigure(handle, new_config):
+    """ Reconfigures the plugin,
+
+    it should be called when the configuration of the plugin is changed during the operation of the South plugin service
+    The new configuration category should be passed.
+
+    Args:
+        handle: handle returned by the plugin initialisation call
+        new_config: JSON object representing the new configuration category
+    Returns:
+        new_handle: new handle to be used in the future calls
+    Raises:
+    """
+
+    # Find diff between old config and new config
+    diff = dict()  # get diff(handle, new_config)
+
+    # Plugin should re-initialize and restart if key configuration is changed
+    # e.g. port / uri / management_host or BLE address etc.
+    if 'port' in diff or 'uri' in diff or 'management_host' in diff:
+        # write necessary code to stop the plugin here
+        new_handle = plugin_init(new_config)
+        new_handle['restart'] = 'yes'
+        _LOGGER.info("Restarting <name> plugin due to change in configuration keys [{}]".format(', '.join(diff)))
+    else:
+        new_handle = copy.deepcopy(handle)
+        new_handle['restart'] = 'no'
+    return new_handle
+
+
+def plugin_shutdown(handle):
+    """ Shutdown the plugin and does required cleanup
+
+    To be called prior to the South service being shut down.
+
+    Args:
+        handle: handle returned by the plugin initialisation call
+    Returns:
+    Raises:
+    """
+    # disconnect the communication with the thing
+    # cleanup
+    _LOGGER.info('<name> poll plugin shut down.')
