f5c6bf08eec713571922ec91426db3e0a01bf9a3
Robert Fox <rfox@osisoft.com>
2018-08-28 13:26:33 +0800

    generalised install script

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 sudo rm -r /usr/local/foglamp/python/foglamp/plugins/south/ds18b20
 
 sudo mkdir /usr/local/foglamp/python/foglamp/plugins/south/ds18b20
-sudo cp ds18b20.py /usr/local/foglamp/python/foglamp/plugins/south/ds18b20
-sudo cp __init__.py /usr/local/foglamp/python/foglamp/plugins/south/ds18b20
+sudo cp *.py /usr/local/foglamp/python/foglamp/plugins/south/ds18b20
 
 curl -sX POST http://localhost:8081/foglamp/service -d '{"name": "ds18b20", "type": "south", "plugin": "ds18b20", "enabled": true}'
 echo "\n"
